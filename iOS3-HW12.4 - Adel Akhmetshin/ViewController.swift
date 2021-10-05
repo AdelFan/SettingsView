f75a37b38c950963a55070c12150b24d31382856
Drdev <fallen.bmxer@gmail.com>
2021-10-05 12:51:12 +0300

    Initial Commit

---
+++ iOS3-HW12.4 - Adel Akhmetshin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS3-HW12.4 - Adel Akhmetshin
+//
+//  Created by Адель Ахметшин on 05.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
